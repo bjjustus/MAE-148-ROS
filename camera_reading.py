599cfc45a2072e4a2bf9bd2ca39724c07dd8db87
bjjustus <75348793+bjjustus@users.noreply.github.com>
2020-12-06 00:09:06 -0800

    Update camera_reading.py

--- camera_reading.py
+++ camera_reading.py
@@ -4,31 +4,34 @@ import rospy, cv2, cv_bridge, numpy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 
-class Follower:
+def __init__(self):
+        self.cam=cv2.VideoCapture(0)
+        cv2.namedWindow("window", 1)
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, q$
+def callback(self,data):
+        hsv = cv2.cvtColor(self.cam, cv2.COLOR_BGR2HSV)
+        lower=numpy.array((0, 50, 50))
+        upper=numpy.array((50, 255, 255))
+        mask=cv2.inRange(hsv, lower, upper)
+        h,w,d= image.shape
+        top=3*4/h
+        bottom=h/4
+        mask[0:bottom,0:w]=0  
+	mask[top:h,0:w]=0		   
+	M=cv2.moments(mask)
+        if M>0:
+           cx= int(M['m10']/M['m00'])
+           cy= int(M['m01']/M['m00'])
+           cv2.circle(self.cam, (cx, cy), 20, (0,0,255), -1)
+           err=cx-w/2
+           self.twist.linear.x=.1
+           self.twist.angular.z=-float(err)/100
 
-        def __init__(self):
-		self.cam=cv2.VideoCapture(0)
-		cv2.namedWindow("window", 1)
-		self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
-	def callback(self,data):
-		hsv = cv2.cvtColor(self.cam, cv2.COLOR_BGR2HSV)
-		lower=numpy.array((0, 50, 50))
-		upper=numpy.array((50, 255, 255))
-		mask=cv2.inRange(hsv, lower, upper)
-		h,w,d= image.shape
-		top=3*4/h
-		bottom=h/4
-		mask[0:bottom,0:w]=0
-		mask[top:h,0:w]=0
-		M=cv2.moments(mask)
-		if M>0:
-		   cx= int(M['m10']/M['m00'])
-		   cy= int(M['m01']/M['m00'])
-		   cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-		   err=cx-w/2
-		   self.twist.linear.x=.1
-		   self.twist.angular.z=-float(err)/100
-		cv2.imshow("window", image)
+        cv2.imshow("window", self.cam)
+        cv2.imshow("mask",mask)
+        cv2.waitKey(0)
 rospy.init_node('line_follower')
-follower = Follower()
 rospy.spin()
+
+					   
+					   
