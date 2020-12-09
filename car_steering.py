b2e709b3039de5975746833a1201a8527627a894
zhongyu95 <60737855+zhongyu95@users.noreply.github.com>
2020-12-08 19:04:01 -0800

    Add files via upload

--- car_steering.py
+++ car_steering.py
@@ -2,9 +2,13 @@
 
 import rospy, math
 from geometry_msgs.msg import Twist
+from adafruit_servokit import ServoKit
+import adafruit_motor.servo
+
+
 
 def __init__(self):
-    self.speed=rospy.Subscriber('cmd_vel_mux/input/teleop', Twist, callback)    
+    self.speed=rospy.Subscriber('cmd_vel_mux/input/teleop', Twist, callback)
 
 def convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase):
     if omega == 0 or v == 0:
@@ -13,7 +17,7 @@ def convert_trans_rot_vel_to_steering_angle(v, omega, wheelbase):
     return math.atan(wheelbase / radius)
 
 def callback(data):
-   rospy.loginfo(data)
+   rospy.loginfo(data.data)
 
 while not rospy.is_shutdown():
     rospy.init_node('car')
@@ -21,16 +25,15 @@ while not rospy.is_shutdown():
     neutral = 370
     PWM_max = 460
     PWM_min = 290
-    k = 10
     msg=Twist()
-    sub=rospy.Subscriber('cmd_vel_mux/input/teleop', Twist, callback)
+    rospy.Subscriber('cmd_vel_mux/input/teleop', Twist, callback)
     x=msg.linear.x
-    z=msg.angular.z
+    z=msg.linear.z
     steering = convert_trans_rot_vel_to_steering_angle(x, z, wheelbase)
-    PWM_steering = neutral + k * steering
-    if PWM_steering > PWM_max:
-        PWM_steering = PWM_max
-    elif PWM_steering < PWM_min:
-        PWM_steering = PWM_min
+    print(x)
 
- 
+    kit = ServoKit(channels=16)
+    servo = adafruit_motor.servo.Servo(servo_channel)
+    kit.servo[1].set_pulse_width_range(290, 460)
+    kit.servo[1].angle = 57.2958 * steering
+    kit.continuous_servo[0].throttle = linear.x
