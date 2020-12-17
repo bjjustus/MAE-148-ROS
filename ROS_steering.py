fb7b1ee19916dca2acf4451e52c4f80009c90c11
zhongyu95 <60737855+zhongyu95@users.noreply.github.com>
2020-12-16 19:21:42 -0800

    Add files via upload

---
+++ ROS_steering.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rospy, math
+from geometry_msgs.msg import Twist
+from adafruit_servokit import ServoKit
+import adafruit_motor.servo
+
+msg=Twist()
+#initialize kit
+kit = ServoKit(channels=16) 
+
+#def convert_trans_rot_vel_to_steering_angle(omega):
+#    #turns angular.z value into an angle
+#    return 12*omega+90
+
+def callback(msg):
+#    x=msg.linear.x
+    z=msg.angular.z
+    steering = 13*z+90
+    print(steering)
+    #add steering
+    kit.servo[1].angle = steering
+def driver():
+    #subscriber gets cmd_vel values from open cv file
+    rospy.init_node('car')
+    rospy.Subscriber('/cmd_vel', Twist, callback)
+    kit.continuous_servo[2].throttle = 0.1
+    rospy.spin()
+    
+	
+#insert throttle
+driver()
+#if rospy.is_shutdown():
+   #kit.continuous_servo[2].throttle = 0
+
